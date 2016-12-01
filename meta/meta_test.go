d0f5ed73652b34a7a44846276cdb78da9df06510
Fahad Ibnay Heylaal <fahad19@gmail.com>
2016-12-01 10:37:18 +0100

    tests for meta.

--- meta/meta_test.go
+++ meta/meta_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 )
 
-func TestAdd(t *testing.T) {
+func TestSet(t *testing.T) {
 	m := New()
 	m.Set("foo", "bar")
 
@@ -13,5 +13,58 @@ func TestAdd(t *testing.T) {
 
 	if expected != actual {
 		t.Errorf("expected %s, actual %s", expected, actual)
+	}
+}
+
+func TestGet(t *testing.T) {
+	m := New()
+	m.Set("foo", "bar")
+
+	expected := "bar"
+	actual := m.Get("foo")
+
+	if expected != actual {
+		t.Errorf("expected %s, actual %s", expected, actual)
+	}
+}
+
+func TestRemove(t *testing.T) {
+	m := New()
+	m.Set("foo", "bar")
+	m.Remove("foo")
+
+	expected := ""
+	actual := m.Get("foo")
+
+	if expected != actual {
+		t.Errorf("expected %s, actual %s", expected, actual)
+	}
+}
+
+func TestGetFields(t *testing.T) {
+	var fixtures = []struct {
+		key   string
+		value string
+	}{
+		{"foo", "bar"},
+		{"baz", "qux"},
+		{"hello", "world"},
+	}
+
+	m := New()
+
+	for _, fixture := range fixtures {
+		m.Set(fixture.key, fixture.value)
+	}
+
+	fields := m.GetFields()
+
+	for _, fixture := range fixtures {
+		expected := fixture.value
+		actual := fields[fixture.key]
+
+		if expected != actual {
+			t.Errorf("expected %s, actual %s", expected, actual)
+		}
 	}
 }
